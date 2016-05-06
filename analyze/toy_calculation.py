f3674f5e19aa538b801ba1dd348a33792c55b797
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-05-06 09:20:29 -0700

    Updated

--- analyze/toy_calculation.py
+++ analyze/toy_calculation.py
@@ -1,51 +1,94 @@
 import pickle as pkl
 import numpy  as np
 import matplotlib.pyplot as plt
+import utils as u
+from pymongo import MongoClient
+import random
+
+def merged_eng(eng_1,eng_2,dr):
+    return eng_1
 
 
-def identity(x):
-    return x
+def mask_eng(eng_1,eng_2,dr):
+    return eng_1
 
 
-def binned_vals(bins_new,binned_dist,bin_multiplier,trans_func = identity,spacing = 0):
-    ret_dist,binned_dist = [],np.array(binned_dist)
-
-    for id,bin in enumerate(bins_new):
-        if id           == len(bins_new) -1 : break
-        bin_low,bin_high = bin,bins_new[id+1]
-
-        temp_bin        = binned_dist[[id for id,x in enumerate(abs(spacing - trans_func(binned_dist[:,0]) * bin_multiplier)) if bin_low < x  < bin_high ]]
-        ret_dist.append(np.sum(temp_bin[:,1]))
-    return ret_dist
+def smear_eng(eng,pdgid):
+    return eng
 
 
-gev_to_mev,detector_distance,electron_spacing,neighbor_prob\
-               = 1000,350,25,.01
+def propogate(x,y,phi,theta,distance):
+    r = distance* np.tan(theta)
+    x_ret = x + r*np.cos(phi)
+    y_ret = y + r*np.sin(phi)
+
+    return x_ret,y_ret
 '''
-The goal is to simulate the probability that a bremm'ed photon overlaps with a nearby electron and is undetected.
-
-
-(1) gev_to_mev is a simple conversion to help relate binning
-(2) detector_distance is the proposed distance from the detector to the target, in mm.
-(3) electron_spacing is the spacing between the incoming electron and it's nearby neighbors
-(4) neighbor_prob  is the probability for a random point picked at a radius of electron_spacing to contain a nearby beam-line electron/
-
-Radial prob is something I cooked up because even if a hard photon radiates at the distance of electron spacing from the inc. electron it will still need to overlap with a nearby neighbor.
-
+def propogate(x,y,phi,theta,distance):
+    return x,y
 '''
-sim            = pkl.load( open( "sim.pkl", "rb" ) )
-eng_dist       = binned_vals(sim['eff_tuple'][1],sim['eng_dist'],gev_to_mev)
-print np.array(sim['ang_dist'])
-sep_dist       = binned_vals(sim['eff_tuple'][0],sim['ang_dist'],detector_distance,np.sin,electron_spacing)
-missed_frac    = neighbor_prob*np.dot(sep_dist,np.dot(eng_dist,1-sim['eff_tuple'][2]))
-print  'The fraction of photons which merge undetected is %s ' %(missed_frac)
+def sep_matrix(tmp_):
+    ret_mat = []
+    for particle in tmp_:
+        x = particle[0]
+        y = particle[1]
+        sep = np.sqrt(np.power((tmp_[:,0] - x),2) + np.power((tmp_[:,1] - y),2))
+        sep[sep == 0] =  1e6
+        ret_mat.append(sep)
+    return np.array(ret_mat)
 
-bins = np.linspace(1,30,90)
 
-eff_matrix = np.dot(eng_dist,1-sim['eff_tuple'][2])
+def build_events(beam_std,bunch_size,n_events,detector_distance,cut,dead_time=5):
+    client      = MongoClient()
+    db          = client['hgcal']
+    events      = []
+    unmerg,merg,merg_eng,phot_merg_eng = [],[],[],[]
+    for counter,item in enumerate(db.target.find().limit( n_events )):
+        tmp_ = []
+        for id,particle in enumerate(item['particles']):
+            if particle['eng'] < cut : continue
 
-plt.semilogy(bins,[np.dot(binned_vals(sim['eff_tuple'][0],sim['ang_dist'],detector_distance,np.sin,distance),eff_matrix) for distance in bins],'ro')
-plt.xlabel('Electron Separation, r',size=20)
-plt.ylabel('Merging Probabiilty/Electron Density',size=20)
-plt.title('Geant4 Sim.',size=16,loc='left')
-plt.savefig('ele_rejection.png')
+            #x_vtx,y_vtx   = particle['x_pos']+np.random.normal(0,beam_std),particle['y_pos']+np.random.normal(0,beam_std)
+            x_vtx,y_vtx   = np.random.normal(0,beam_std),np.random.normal(0,beam_std)
+            x_cord,y_cord = propogate(x_vtx,y_vtx,particle['phi'],particle['theta'],detector_distance)
+            tmp_.append([x_cord,y_cord,particle['pdg'],particle['eng'],dead_time])
+        if len(tmp_) == 0:  continue
+
+        if len(events) > 0: events = np.vstack((tmp_,events[events[:,-1] > 0]))
+        else: events = np.array(tmp_)
+
+        if counter < 10: continue
+        if counter%bunch_size == 0:
+            if len(events) > 0: events[:,-1] = events[:,-1]  - 1
+
+            #sep_mat = sep_matrix(events)
+            evt_sub = events[events[:,-1]==dead_time-1.0]
+            sep_sub = sep_matrix(evt_sub)
+            for id,ele in enumerate(evt_sub):
+
+                if evt_sub[id][2] != 22.: continue
+                argmin = np.argmin(sep_sub[id])
+                if sep_sub[id][argmin] > 20.:
+                    unmerg.append(1)
+                else:
+                    merg.append(1)
+                    merg_eng.append(evt_sub[argmin][3] )
+                    phot_merg_eng.append(evt_sub[id][3])
+                #dr.append(sep_sub[id][argmin])
+
+    return np.array(merg),np.array(unmerg)
+detector_distance,beam_std,bunch_size,measurement_cut,n_events\
+               = 250,21,2,2000,2000
+
+sim            = pkl.load( open( "../output/pkl/eff.pkl", "rb" ) )
+in_eff         = 1-sim['eff_tuple'][2]
+eff = []
+for detector_distance in [2.5,5.,7.5,10.,12.5,15.,17.5,20.,22.5,25.,27.5,30.,32.5,35.]:
+    merg,unmerg       = build_events(beam_std,bunch_size,n_events,detector_distance,measurement_cut)
+    eff.append(float(len(merg))/(len(merg)+len(unmerg)))
+
+plt.plot([2.5,5.,7.5,10.,12.5,15.,17.5,20.,22.5,25.,27.5,30.,32.5,35.],eff)
+plt.xlabel('Detector Distance',fontsize=20)
+plt.ylabel('Fraction of Merged Photons',fontsize=20)
+plt.savefig('../output/plots/merge_vs_std.png')
+plt.show()
