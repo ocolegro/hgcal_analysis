0b5ab90e70baed982392055eadce397c3e62f7ba
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-05-13 09:20:05 -0700

    updatd

--- analyze/smear_factors.py
+++ analyze/smear_factors.py
@@ -5,14 +5,16 @@ import smear_functions as sf
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
 
-n_events  = 200000
+n_events  = 1000000
 ##Load up specified number of events from MongoDB
 client      = MongoClient()
 db          = client['hgcal']
 
 
+arr = u.prep_mask(u.load_event_array(db.electrons_finer.find(no_cursor_timeout=True).limit(n_events)),
+                   u.load_event_array(db.electrons_finer.find(no_cursor_timeout=True).limit(n_events)),500000)
 
-
+'''
 arr       = u.prep_smear(u.load_event_array(db.electrons_finer.find(no_cursor_timeout=True).limit(n_events)),False,[11.])
 sf.single_smear(arr[:,2],arr[:,3],'single_electron')
 
@@ -24,7 +26,6 @@ sf.merged_smear(arr[:,1],arr[:,2],arr[:,3],'electron_photon')
 
 
 
-'''
 arr = u.prep_mask(u.load_event_array(db.photons.find(no_cursor_timeout=True).limit(100000)),
                    u.load_event_array(db.electrons.find(no_cursor_timeout=True).limit(100000)),n_events)
 print 'Done'
